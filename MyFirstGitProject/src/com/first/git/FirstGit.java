321af0ab33205e715a5840e7f1cd98b136e430b8
Rajkumar <rajkumar.anthoniraj@gmail.com>
2014-12-10 19:40:15 +0530

    Initial commit

---
+++ MyFirstGitProject/src/com/first/git/FirstGit.java
@@ -0,0 +1,18 @@
+package com.first.git;
+
+public class FirstGit {
+
+	public FirstGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is rocking");
+
+	}
+
+}
